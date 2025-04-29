06f809d007b9ea1f9bc4f0472e2299b47d9f8c7c
DugtheDog1 <154391896+DugtheDog1@users.noreply.github.com>
2025-04-29 22:14:34 +0000

    is_day

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # The homepage renders the 'index.html' template
+    # This will render the 'index.html' file located in the 'templates' folder
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Set host to '0.0.0.0' to allow access via a custom IP address or external interface
+    app.run(host='0.0.0.0', port=5000, debug=True)
