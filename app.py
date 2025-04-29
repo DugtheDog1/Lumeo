73c80f4a784bc936fadb534e39534fb189ef2103
DugtheDog1 <154391896+DugtheDog1@users.noreply.github.com>
2025-04-29 22:36:51 +0000

    more

--- app.py
+++ app.py
@@ -1,37 +1,22 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    # This will render the 'index.html' file located in the 'templates' folder
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    # Set host to '0.0.0.0' to allow access via a custom IP address or external interface
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-import requests
-
-#Hide warnings ----
+# Hide warnings
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-#------------------
 
-def index():
+@app.route('/')
+def home():
     url = "https://api.open-meteo.com/v1/forecast?latitude=44.8041&longitude=-93.1669&hourly=soil_moisture_1_to_3cm,soil_temperature_6cm&current=temperature_2m,is_day,showers,snowfall,rain,precipitation,weather_code,cloud_cover"
-    response = requests.get(url, verify=False)
-    data = response.json()
+    
+    response = requests.get(url, verify=False) #Open link, verify is false due to the thing and the import urllib3 and .disable_warnings is to ignore the message
+    data = response.json() # Convert to json 
 
-    # Get the "is_day" data (whether it's daytime or not)
-    is_day_data = data["current"].get("is_day", None)
+    is_day_data = data["current"].get("is_day", None) #Goes through the data and looks for current/is_day value in dictionary thing
 
-    # Pass the data to the HTML page
-    return render_template("index.html", is_day=is_day_data)
+    return render_template("index.html", is_day=is_day_data) #Think global var, exports data to the html page
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-index()
+    app.run(host='0.0.0.0', port=5000, debug=True)
