7b63d53fb8f1cc36c1f5ac1d81b759b712fced48
DugtheDog1 <154391896+DugtheDog1@users.noreply.github.com>
2025-04-30 00:01:06 +0000

    Commit

--- app.py
+++ app.py
@@ -8,15 +8,23 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 @app.route('/')
-def home():
+
+
+def get_current_info():
     url = "https://api.open-meteo.com/v1/forecast?latitude=44.8041&longitude=-93.1669&hourly=soil_moisture_1_to_3cm,soil_temperature_6cm&current=temperature_2m,is_day,showers,snowfall,rain,precipitation,weather_code,cloud_cover"
     
     response = requests.get(url, verify=False) #Open link, verify is false due to the thing and the import urllib3 and .disable_warnings is to ignore the message
     data = response.json() # Convert to json 
 
-    is_day_data = data["current"].get("is_day", None) #Goes through the data and looks for current/is_day value in dictionary thing
+    current_is_day = data["current"].get("is_day", None) #Goes through the data and looks for current/is_day value in dictionary thing
+    current_time = data["current"].get("time", None) #
+    current_percipitation_status = data["current"].get("time", None)
+    current_overall_status = data["current"].get("weather_code", None)
 
-    return render_template("index.html", is_day=is_day_data) #Think global var, exports data to the html page
+
+
+
+    return render_template("index.html", isDay=current_is_day, currentTime=current_time, currentPercipitationStatus=current_percipitation_status, currentOverallStatus=current_overall_status) #Think global var, exports data to the html page
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
